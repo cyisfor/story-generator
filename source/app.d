a8ef01f09de2b42a6cd30bdd1b0f08ce88304304
Cy <nope>
2016-03-21 17:37:39 +0000

    auto (/home/creation/stories/code/generate.d/source/app.d) 131 3313

---
+++ source/app.d
@@ -0,0 +1,150 @@
+import htmlderp: querySelector, createElement;
+static import git;
+static import db;
+import reindex: reindex, chapter_name;
+
+import makers : makers, Maker;
+static import nada = makers.birthverse;
+static import nada2 = makers.littlepip;
+static import htmlish;
+
+import html: Document;
+
+import std.algorithm.searching: endsWith;
+import std.stdio: writeln, writefln;
+import std.file: write, mkdir, timeLastModified, readText, exists, chdir,
+  FileException;
+import std.path: buildPath;
+import std.conv: to;
+import std.datetime : SysTime;
+
+import core.memory: GC;
+
+db.Story[string] stories;
+
+struct Update {
+  SysTime modified;
+  int which;
+  string location;
+  bool is_hish;
+  void update() {
+	db.Story story;
+	if(location in stories) {
+	  story = stories[location];
+	} else {
+	  story = db.story(location);
+	  stories[location] = story;
+	}
+	// find a better place to put stuff so it doesn't scram the source directory
+	try {
+	  mkdir(buildPath(location,"base"));
+	} catch {
+	  writeln("beep");
+	}
+	Maker maker;
+	if(location in makers) {
+	  maker = makers[location];
+	} else {
+	  maker = htmlish.check;
+	}
+
+	string ext;
+	if( is_hish ) {
+	  ext = ".hish";
+	} else {
+	  ext = ".txt";
+	}
+	
+	auto name = "chapter" ~ to!string(which);
+	auto markup = buildPath(location,"markup",name ~ ext);
+
+	auto chapter = story[which];
+
+	try {
+	  if(timeLastModified(markup) <= chapter.modified) return;
+	} catch(FileException) {
+	  return;
+	}
+	
+	auto base = buildPath(location,"base",name ~ ".xhtml");
+	maker.make(markup,base);
+	Document doc;
+	doc = new Document(readText("base/" ~
+									chapter_name(chapter.which) ~
+									".xhtml"));
+
+	auto head = querySelector(doc,"head");
+	auto links = querySelector(doc,"#links");
+	bool didlinks = false;
+	void dolink(string href, string rel, string title) {
+	  auto link = doc.createElement("link",head);
+	  link.attr("rel",rel);
+	  link.attr("href",href);
+	  if( didlinks ) {
+		links.appendText(" ");
+	  } else {
+		didlinks = true;
+	  }
+	  link = doc.createElement("a",links);
+	  link.attr("href",href);
+	  link.appendText(title);
+	}
+	if( chapter.which > 0 ) {
+	  dolink(chapter_name(chapter.which-1), "prev", "Previous");
+	}
+	if( chapter.which + 1 < story.chapters ) {
+	  dolink(chapter_name(chapter.which+1), "next", "Next");
+	}
+
+	maker.chapter(doc);
+	chapter.update(modified, which, to!string(querySelector(doc,"title").html));
+
+	write("out/" ~
+		  chapter_name(chapter.which) ~
+		  ".xhtml",doc.root.outerHTML);
+	
+	string chapterTitle = to!string(querySelector(doc,"title").text);
+	story[which].update(modified, which, chapterTitle);
+  }
+}
+
+Update[string] queue;
+string only_location = null;
+
+void check_chapter(SysTime modified, int which, string location, bool is_hish) {
+  if(!exists(location)) return;
+  if(only_location && (!location.endsWith(only_location))) return;
+
+  string key = location ~ "/" ~ to!string(which);
+  if(key in queue) return;
+  queue[key] = Update(modified,which,location,is_hish);
+}
+
+
+void derpmain(string[] args)
+{
+  string since = "HEAD~20..HEAD";
+  while(!exists(".git")) {
+	writeln("boing");
+	chdir("..");
+  }
+  if(args.length > 1) {
+	since = args[1];
+	if(since=="") since = null;
+  }
+  writeln("since "~since);
+  git.parse_log(since,&check_chapter);
+  foreach(update; queue.values) {
+	writeln("checking ",update.location);
+	update.update();
+  }
+  reindex(stories);
+  db.close();
+}
+
+
+void main(string[] args) {
+  derpmain(args);
+  GC.collect();
+  writeln("yay");
+}
