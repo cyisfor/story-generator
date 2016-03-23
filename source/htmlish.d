d98e08918c2a80a1852c4c1f3f4cdeb0791cdca6
user <nobody>
2016-03-23 06:15:54 +0000

    auto (/home/creation/stories/code/generate.d/source/htmlish.d) 66 1326

--- source/htmlish.d
+++ source/htmlish.d
@@ -1,22 +1,64 @@
 import std.stdio: File,writeln;
 import std.file: rename;
 import std.process: spawnProcess, wait;
+import std.socket: Socket, UnixAddress;
 
-const string exe = "/home/code/htmlish/parse";
+immutable string exe = "code/htmlish/parse";
+immutable string address = "parse.sock";
 
+Socket socket;
+
+class Watcher {
+  Pid pid;
+  
+  this() {
+	pid = spawnProcess([exe,address]);
+  }
+  auto connect() {
+	socket = new Socket(AddressFamily.UNIX,
+						SocketType.STREAM,
+						ProtocolType.RAW);
+	socket.connect(new UnixAddress(address));	
+	return socket;
+  }	  
+  ~this() {
+	pid.kill();
+	pid.wait();
+  }
+}
+
+void copy(File source, Socket dest) {
+  static void[0x1000] buf;
+  scope(exit) {
+	source.close();
+  }
+  for(;;) {
+	auto chunk = source.rawRead(buf);
+	if( chunk.length == 0 ) break;
+	dest.send(chunk);
+  }
+}
+
+void copy(Socket source, File dest) {
+  static void[0x1000] buf;
+  scope(exit) {
+	// om nom nom
+	dest.close();
+  }
+  for(;;) {
+	auto amt = source.receive(buf);
+	if( amt == 0 ) break;
+	dest.rawWrite(buf[0..amt]);
+  }
+}
+	if( chunk.length == 0 ) break;
+Watcher watcher;
 void make(string src, string dest) {
 	writeln("htmlishhhhh ",dest);
 	File source = File(src,"r");
 	File destination = File(dest~".temp","w");
-	static import std.stdio;
-	assert(0==
-		   spawnProcess(exe,
-						source,
-						destination,
-						std.stdio.stderr,
-						["template": "template/chapter.xhtml",
-						 "title": "???"]).wait());
+	auto socket = watcher.connect();
+	copy(source,socket);
+	copy(socket,destination);
 	rename(dest~".temp",dest);
-	source.close();
-	destination.close();
 }
