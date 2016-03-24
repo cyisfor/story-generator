6ee03eac251c1635ac4a685253ab66ee8500ecc9
user <nobody>
2016-03-24 22:02:02 +0000

    auto (/home/creation/stories/code/generate.d/source/htmlish.d) 116 2452

--- source/fuck_selectors.d
+++ source/fuck_selectors.d
@@ -1,24 +1,35 @@
-auto for_all!(bool depth_first = true)(auto parent) {
+auto for_all!(bool depth_first = false)(auto parent) {
   import std.range: chain;
   import std.algorithm.iteration: map, joiner, filter;
-  if(depth_first) {
+  static if(depth_first) {
 	return chain(map!for_all(parent.children).joiner,[parent]);
   } else {
-	return chain([parent],map!for_all(parent.children).joiner);
+	return chain([parent],parent.descendants);
   }
 }
-auto is_element(auto range) {
-  return range.filter!((e) => e.isElementNode);
+
+auto maybe4all(auto mayberange) {
+  import std.range.primitives: isInputRange;
+  static if( isInputRange(mayberange) ) {
+	return mayberange;
+  } else {
+	return for_all(maybe);
+  }
 }
+
+auto is_element(auto range) {
+  return maybe4all(range).filter!((e) => e.isElementNode);
+}
+
 auto by_name(HTMLstring name)(auto range) {
-  return range.filter!((e) => e.name == name);
+  return maybe4all(range).is_element.filter!((e) => e.name == name);
 }
 auto by_id(HTMLstring ident)(auto range) {
-  return range.filter!((e) => e.attr("id") == ident);
+  return maybe4all(range).is_element.filter!((e) => e.attr("id") == ident);
 }
 auto by_class(HTMLstring ident)(auto range) {
-  return range.filter!((e) => e.attr("class") == ident);
+  return maybe4all(range).is_element.filter!((e) => e.attr("class") == ident);
 }
 auto has_attr(HTMLstring attr)(auto range) {
-  return range.filter!((e) => e.attr(attr) !is null);
+  return maybe4all(range).is_element.filter!((e) => e.attr(attr) !is null);
 }
--- source/htmlish.d
+++ source/htmlish.d
@@ -11,92 +11,39 @@ static this() {
   default_template = createDocument(import("template/default.html"));
 }
 
+import fuck_selectors: for_all, is_element, by_name, by_class, by_id;
 
-
-void process_root(auto root) {
-
+auto process_root(Document dest) {
+  return (auto node) {
+	
+  
 auto parse(HTMLstring source, ref Document templit = null) {
+  import std.algorithm.searching: until;
+  import std.range: chain;
+  import htmlderp: createDocument;
+  
   if(templit == null) {
 	templit = default_template;
   }
-  bool ended_newline = false;
+
   auto dest = templit.clone();
-  auto content = chain(dest.root.by_name("content"),
-					   dest.root.by_name("div")
   
-
-  fuck_selectors.has_attr(src.root,"hish").each(&process_root);
-	
+  // find where we're going to dump this htmlish
+  auto content = until!"a !is null"
+	(chain(dest.root.by_name!"content",
+		   dest.root.by_name!"div".by_id!"content",
+		   dest.root.by_name!"body")).front;
+  if(content is null) {
+	content = dest.createElement("body");
+	dest.root.appendChild(content);
+  }
   
-  class Watcher {
-  Pid pid;
-  bool disabled;
-  void get() {
-	if(pid !is null) return;
-	disabled = false;
-	try {
-	  pid = spawnProcess([exe,address],
-						 [
-						  "template":"template/chapter.xhtml",
-						  "title": "???"
-						  ]);
-	} catch(ProcessException e) {
-	  disabled = true;
-	}
-  }
-  auto connect() {
-	get();
-	assert(!disabled);
-	socket = new Socket(AddressFamily.UNIX,
-						SocketType.STREAM,
-						ProtocolType.IP);
-	print(address);
-	for(;;) {
-	  try {
-		socket.connect(new UnixAddress(address));
-		break;
-	  } catch(SocketOSException e) {
-	  }
-	}
-	return socket;
-  }	  
-  ~this() {
-	if(pid !is null) {
-	  pid.kill();
-	  pid.wait();
-	}
-  }
+  auto src = createDocument(source);
+  assert(src.root);
+
+  content.has_attr!"hish".each!process_root(dest);
 }
 
-void copy(File source, Socket dest) {
-  static void[0x1000] buf = void;
-  scope(exit) {
-	source.close();
-  }
-  for(;;) {
-	auto chunk = source.rawRead(buf);
-	if( chunk.length == 0 ) break;
-	dest.send(chunk);
-  }
-}
-
-void copy(Socket source, File dest) {
-  static void[0x1000] buf = void;
-  scope(exit) {
-	// om nom nom
-	dest.close();
-  }
-  for(;;) {
-	auto amt = source.receive(buf);
-	if( amt == 0 ) break;
-	dest.rawWrite(buf[0..amt]);
-  }
-}
-
-Watcher watcher;
-static this() {
-  watcher = new Watcher();
-}
 void make(string src, string dest) {
 	print("htmlishhhhh ",dest);
 	File source = File(src,"r");
