3ddb1f10f89e6bf6b42aa5152239013d10131bc6
user <nobody>
2016-03-29 06:37:09 +0000

    okay, removed

--- source/htmlderp.d
+++ source/htmlderp.d
@@ -13,25 +13,14 @@ auto ref querySelector(html.Document* doc, html.HTMLString s) {
   return res.front;
 }
 
-deprecated
-auto createDocument(html.HTMLString source) {
-  // no verbose entities in my utf-8 please
-  return html.createDocument(source);
-}
-
 E detach(E)(E e) {
   e.detach();
   return e;
 }
 
-deprecated
-auto htmlderp(NodeType)(NodeType n, html.HTMLString source) {
-  return n.html(source);
-}
-
 unittest {
   import print: print;
-  auto doc = createDocument("It's");
+  auto doc = html.createDocument("It's");
   print(doc.root.html);
   doc.root.html("It's");
   print(doc.root.html);
