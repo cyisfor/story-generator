5d8a4ecf6d2bdfd5f2a25afc6eebc720162af1e4
autocommit <autocommit>
2017-05-16 18:20:59 +0000

    auto 0 198 1797

--- html_when/source/test.d
+++ html_when/source/test.d
@@ -4,6 +4,18 @@ unittest {
 	import print: print;
 	import std.process: environment;
 	print(import("test-example.html"));
-	print("=============================================");
-	print(import("test-example.html"));
-	
+	function doit() {
+		print("=============================================");
+		auto doc = process_when(createDocument(import("test-example.html")));
+		print(doc.root.html);
+	}
+	doit();
+	environment["censored"] = "1";
+	doit();
+	environment["something"] = "serious";
+	doit();
+	environment["something"] = "silly";
+	doit();
+	environment["foo"] = "bar";
+	doit();
+}
