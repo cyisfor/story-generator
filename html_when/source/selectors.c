47c6fdc22531b299b0354b201c4ce8596df41cfc
autocommit <autocommit>
2017-08-08 02:05:05 +0000

    auto 0 31 405

--- html_when/source/selectors.c
+++ html_when/source/selectors.c
@@ -1,3 +1,6 @@
+#include "selectors.h"
+#include <stdlib.h> // malloc...
+
 enum { UP, DOWN, /*LEFT,*/ RIGHT} directions;
 /* Theseus strategy: if last direction was UP, check RIGHT, UP, then done
 	 if last direction was RIGHT, check DOWN, RIGHT, UP
