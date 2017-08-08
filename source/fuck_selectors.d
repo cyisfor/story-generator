d091eccb3e435883461e56f5f0e39ae2624f7139
autocommit <autocommit>
2017-08-08 00:50:34 +0000

    auto 0 206 3201

--- source/fuck_selectors.d
+++ source/fuck_selectors.d
@@ -1,73 +1,12 @@
-import html: HTMLString;
-import std.algorithm.iteration: filter;
-
-auto for_all(bool depth_first = false,T)(T parent) {
-  import std.range: chain;
-  import std.algorithm.iteration: map, joiner, filter;
-  static if(depth_first) {
-	return chain(map!for_all(parent.children).joiner,[parent]);
-  } else {
-	return chain([parent],parent.descendants);
-  }
+GumboNode* next_by_name(GumboNode* cur, const char* name, size_t nlen) {
+	size_t otherlen = strlen(cur->name);
+	if(otherlen == nlen && 0==memcmp(cur->name,name,nlen)) {
+		return cur;
+	}
+	int i;
+	for(i=0;i<cur->v.element.children.length;++i) {
+		GumboNode* next = next_by_name(cur->v.element.children.data[i],name,nlen);
+		if(next) return next;
+	}
+	return NULL;
 }
-
-auto maybe4all(T)(T mayberange) {
-  import std.range.primitives: isInputRange;
-  static if( isInputRange!T ) {
-	return mayberange;
-  } else {
-	return for_all(mayberange);
-  }
-}
-
-auto is_element(T)(T range) {
-  return maybe4all(range).filter!((e) => e.isElementNode);
-}
-
-bool derp(HTMLString name,E)(E e) {
-  return e.tag == name;
-}
-
-auto by_name(HTMLString name,T)(T range) {
-  return maybe4all(range).is_element.filter!(derp!(name,T));
-}
-auto by_id(HTMLString ident,T)(T range) {
-  return maybe4all(range).is_element.filter!((e) => e.attr("id") == ident);
-}
-auto by_class(HTMLString ident,T)(T range) {
-  return maybe4all(range).is_element.filter!((e) => e.attr("class") == ident);
-}
-auto has_attr(HTMLString attr,T)(T range) {
-  return maybe4all(range).is_element.filter!((e) => e.attr(attr) !is null);
-}
-
-unittest {
-  import print: print;
-  import std.array: array;
-  import std.algorithm.iteration: map;
-  import html: createDocument;
-  assert(["a","a","a"] ==
-		 array(createDocument("<a/><a/><a/>")
-			   .root.by_name!"a".map!"a.tag"));
-  assert(["a","a","a"] ==
-		 array(createDocument("<a/><a/><b/><a/>")
-			   .root.by_name!"a".map!"a.tag"));
-  assert(["a","a","a"] ==
-		 array(createDocument(`<a id="1"/><a id="2"/><a id="1"/><a id="1"/>`)
-			   .root.by_id!"1".map!"a.tag"));
-  assert(["a","a","a"] ==
-		 array(createDocument(`<a id="1"/> text in here
-<a id="2">and here</a><a id="1"/><a id="1"/>`)
-			   .root.by_id!"1".map!"a.tag"));
-  assert(["a","a","a"] ==
-		 array(createDocument(`<a foo="1"/><a/><a foo="1"/><a foo="1"/>`)
-			   .root.has_attr!"foo".map!"a.tag"));
-  assert(["a","a","a"] ==
-		 array(createDocument(`<a class="1"/><a class="3"/><a class="1"/><a class="1"/>`)
-			   .root.by_class!"1".map!"a.tag"));
-  assert(createDocument(`<!DOCTYPE html> <html> <head><meta charset="utf-8">
-  <title></title><header></header></head> <body><h1><intitle></intitle></h1>
-  <top></top><content></content><footer></footer> </body></html`)
-		 .root.by_name!"content".empty == false);
-  
-}  
