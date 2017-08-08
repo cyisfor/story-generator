bd19039b47277600b5082ae746e5c2e6cf733567
autocommit <autocommit>
2017-08-08 00:44:37 +0000

    auto 0 175 2586

--- html_when/source/html_when.c
+++ html_when/source/html_when.c
@@ -20,79 +20,59 @@ void html_when(GumboOutput* root) {
 				}
 				if(*name) {
 					const char* match = getenv(name);
-					if(match == NULL) {
+					if(!*a->value) {
+						// without a value, no limitation to making falsity true and vice versa
 						condition = !condition;
 					} else {
-						if(*a->value
-						
-		if(inverted) {
-			gumbo_vector_remove(NULL, inverted, &cur->v.element.attributes);
-		}
-		auto a = e.attrs().keys;
-//		print("DERP",inverted,a);
-		auto b = a[0];
-		if(b[0] == '!') {
-			b = b[1..$].strip();
-//			print("um",b);
-			inverted = !inverted;
-		}
-		auto c = environment.get(b);
-		auto match = e.attr(b);
-//		print("umm",c,match);
-		if(match != "") {
-			match = unescape(match);
-		}
-		bool condition() {
-			if(inverted) {
-				if(match == "") {
-					return c is null;
-				} else {
-					return c != match;
-				}
-			}
-			if(match == "") {
-				return c !is null;
-			}
-			return c == match;
-		}
-		if(condition()) {
-			void subst_vals(NodeType e) {
-				void replace_html(ref NodeType e) {
-					e.html(c);
-					while(e.firstChild) {
-						e.firstChild.insertBefore(e);
+						// a->value is already unescaped
+						if(match && 0==strcasecmp(a->value,match)) {
+							condition = !condition;
+						}
 					}
-					e.destroy();
-				}
-				if(e.tag == "val") {
-					replace_html(e);
-					return;
-				}
-				foreach(arg; e.by_name!"val") {
-					replace_html(arg);
-					while(arg.firstChild) {
-						arg.firstChild.insertBefore(arg);
-					}
-					arg.destroy();
 				}
 			}
-			while(e.firstChild && e.firstChild.tag != "else") {
-				subst_vals(e.firstChild);
-				e.firstChild.insertBefore(e);
+		}
+		GumboVector* kids = &cur->v.element.children;
+		int elsepoint = -1;
+		if(condition) {
+			// positive when clause
+			// find else, if we can
+			for(i=0;i<kids->length;++i) {
+				GumboNode* kid = (GumboNode*) kids->data[i];
+				if(strcasecmp(kid->name,"else")) {
+					// remove this, and all the rest after.
+					elsepoint = i;
+				}
+				if(elsepoint >= 0) {
+					gumbo_destroy_node(&kDefaultOptions, kid);
+				}
+			}
+			if(elsepoint >= 0) {
+				kids->length = elsepoint;
 			}
 		} else {
-			while(e.firstChild) {
-				if(e.firstChild.tag == "else") {
-					e.firstChild.destroy();
-					while(e.firstChild) {
-						e.firstChild.insertBefore(e);
-					}
-				} else {
-					e.firstChild.destroy();
+			// negative when clause
+			// try to find else...
+			for(i=0;i<cur->v.element.children.length;++i) {
+				GumboNode* kid = (GumboNode*) cur->v.element.children.data[i];
+				if(strcasecmp(kid->name,"else")) {
+					elsepoint = i;
+					gumbo_destroy_node(&kDefaultOptions, kid);
+					break;
 				}
+				gumbo_destroy_node(&kDefaultOptions, kid);				
+			}
+			// move everything past elsepoint down to 0
+			if(elsepoint == -1) {
+				cur->v.element.children.length = 0;
+			} else {
+				int newlen = kids->length - elsepoint - 1;
+				memmove(kids->data,
+								kids->data + elsepoint + 1,
+								sizeof(*kids->data) * newlen);
+				kids->length = newlen;
 			}
 		}
-		e.destroy();
 	}
 }
 
