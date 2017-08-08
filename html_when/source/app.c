c7cc90d6b9182716a4e287c9cb275b1e6e8c0fdf
autocommit <autocommit>
2017-08-08 03:06:09 +0000

    auto 0 33 483

--- html_when/source/app.c
+++ html_when/source/app.c
@@ -9,7 +9,7 @@ int main(int argc, char**argv) {
 
 	struct stat info;
 	ensure0(fstat(0,&info));
-	const char* buf = mmap(NULL,info.st_size,PROT_READ,MAP_PRIVATE,0,0);
+	char* buf = mmap(NULL,info.st_size,PROT_READ,MAP_PRIVATE,0,0);
 	ensure_ne(MAP_FAILED,buf);
 	GumboOutput* out = gumbo_parse_with_options(&kGumboDefaultOptions, buf, info.st_size);
 	munmap(buf,info.st_size);
