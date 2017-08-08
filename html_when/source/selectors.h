8f70cb3e94e6d527af4033862830229090b5ad92
autocommit <autocommit>
2017-08-08 02:54:53 +0000

    auto 0 80 763

--- html_when/source/selectors.h
+++ html_when/source/selectors.h
@@ -1,3 +1,18 @@
 #include "gumbo.h"
 
+// for moving up, has to keep a stack of cpos's...
+// can't use call stack because next gets re-called... function pointers?
+typedef bool (*Checker)(GumboNode*,void*);
+struct Selector {
+	int* data; // make sure to init = {}
+	size_t n;
+	size_t space;
+	void* udata;
+	Checker check;
+	GumboNode* cur;
+};
+
+void find_start(struct Selector* s, GumboNode* top, Checker check, void* udata);
+GumboNode* find_next(struct Selector* pos);
+
 GumboNode* next_by_name(GumboNode* cur, const char* name, size_t nlen);
