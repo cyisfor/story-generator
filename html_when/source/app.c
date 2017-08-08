13ea0cfcd4beec596a7be4b536eaeb6acdc3f141
autocommit <autocommit>
2017-08-08 03:06:48 +0000

    auto 0 28 391

--- html_when/source/app.c
+++ html_when/source/app.c
@@ -14,5 +14,5 @@ int main(int argc, char**argv) {
 	GumboOutput* out = gumbo_parse_with_options(&kGumboDefaultOptions, buf, info.st_size);
 	munmap(buf,info.st_size);
 	html_when(out->document);
-	output(1,out);
+	output(1,out->document);
 }
