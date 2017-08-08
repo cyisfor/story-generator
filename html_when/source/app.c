33ad60c9276f299d89a9f3bbe366acd7d93f78ed
autocommit <autocommit>
2017-08-08 03:53:11 +0000

    auto 0 28 403

--- html_when/source/app.c
+++ html_when/source/app.c
@@ -13,8 +13,8 @@ int main(int argc, char**argv) {
 	char* buf = mmap(NULL,info.st_size,PROT_READ,MAP_PRIVATE,0,0);
 	ensure_ne(MAP_FAILED,buf);
 	GumboOutput* out = gumbo_parse_with_options(&kGumboDefaultOptions, buf, info.st_size);
-	munmap(buf,info.st_size);
 	html_when(out->document);
 	int dest = open("/tmp/output.deleteme",O_WRONLY|O_CREAT|O_TRUNC,0644);
 	output(dest,out->document);
+	munmap(buf,info.st_size);
 }
