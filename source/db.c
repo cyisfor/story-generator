2a04799912dbd7feaf1eb2380edaa19dc04e7394
autocommit <autocommit>
2017-08-10 19:06:16 +0000

    auto 0 433 5365

--- source/db.c
+++ source/db.c
@@ -1,21 +1,78 @@
+#define PREPARE(name,stmt) {																	 \
+	db_check(sqlite3_prepare_v2(db, LITLEN(stmt), &name, NULL)); \
+	add_stmt(name);																							 \
+	}
+
+#define DECLARE_STMT(name,stmt)																		\
+	static sqlite3_stmt* name = NULL;																\
+	if(name == NULL) {																							\
+		PREPARE(name, stmt);																					\
+	}
+
+sqlite3* db = NULL;
+
+static void db_check(int res) {
+	switch(res) {
+	case SQLITE_OK:
+	case SQLITE_DONE:
+	case SQLITE_ROW:
+		return;
+	};
+	
+	if(!db) {
+		error(23,0,"no db %d %s",res, sqlite3_errstr(res));
+	}
+	error(42,0,"db error %d(%s) %s",
+				res,sqlite_errstr(res),
+				sqlite3_errmsg(db));
+}
+
+void db_open(const char* filename) {
+	db_check(sqlite3_open(filename,&db));
+	assert(db != NULL);
+}
+
+/* since sqlite sucks, have to malloc copies of pointers to all statements,
+	 since the database won't close until we finalize them all. */
+sqlite3_statement** stmts = NULL;
+size_t nstmt = 0;
+static void add_stmt(sqlite3_stmt* stmt) {
+	stmts = realloc(stmts,++nstmt);
+	stmts[nstmt-1] = stmt;
+}
+
+static sqlite3_stmt* begin(void) {
+	DECLARE_STMT(stmt,"BEGIN");
+	return stmt;
+}
+static sqlite3_stmt* commit(void) {
+	DECLARE_STMT(stmt,"COMMIT");
+	return stmt;
+}
+static sqlite3_stmt* rollback(void) {
+	DECLARE_STMT(stmt,"ROLLBACK");
+	return stmt;
+}
+
+void db_close(void) {
+	size_t i;
+	for(i=0;i<nstmt;++i) {
+		db_check(sqlite3_finalize(stmts[i]));
+
 #define DB_OID(o) o.id
 typedef unsigned char db_oid[GIT_OID_RAWSZ]; // to .h
 
 void db_saw_commit(git_time_t timestamp, db_oid commit) {
-	static sqlite3_stmt *insert = NULL;
-	if(insert == NULL) {
-		PREPARE(insert,"INSERT OR REPLACE INTO commits (oid,timestamp) VALUES (?,?)");
-	}
+	DECLARE_STMT(insert,"INSERT OR REPLACE INTO commits (oid,timestamp) VALUES (?,?)");
 	sqlite3_bind_blob(insert, 1, commit, sizeof(commit), NULL);
 	sqlite3_bind_int(insert, 2, timestamp);
 	db_once(insert);
 }
 
+
 bool db_last_seen_commit(db_oid commit) {
-	static sqlite3_stmt *find = NULL;
-	if(find == NULL) {
-		PREPARE(find,"SELECT oid FROM commits ORDER BY timestamp DESC LIMIT 1");
-	}
+	DECLARE_STMT(find,"SELECT oid FROM commits ORDER BY timestamp DESC LIMIT 1");
+	
 	int res = sqlite3_step(find);
 	switch(res) {
 	case SQLITE_DONE:
@@ -32,4 +89,16 @@ bool db_last_seen_commit(db_oid commit) {
 	};
 }
 	
-	
+void db_saw_chapter(bool deleted, string location, long int chapnum) {
+	identifier story = db_find_story(location);
+	if(deleted) {
+		DECLARE_STMT(delete, "DELETE FROM chapters WHERE story = ? AND chapter = ?");
+		sqlite3_bind_int64(delete,1,story);
+		sqlite3_bind_int64(delete,2,chapnum);
+		db_once(delete);
+	} else {
+		DECLARE_STMT(insert, "INSERT INTO chapters WHERE story = ? AND chapter = ?");
+		sqlite3_bind_int64(insert,1,story);
+		sqlite3_bind_int64(insert,2,chapnum);
+		db_once(insert);
+	}
--- source/main.c
+++ source/main.c
@@ -70,78 +70,29 @@ int main(int argc, char *argv[])
 
 	create_setup();
 
-	bool on_chapter(git_time_t timestamp,
-									long int chapnum,
-									const string loc,
-									const string name) {
-
-		/*printf("saw %d of ",chapnum);
-		STRPRINT(loc);
-		fputc('\n',stdout);*/
-		// lookup location
-		string* testloc = bsearch(&loc,locations,nloc,sizeof(*locations),
-															 (void*)compare_loc);
-		char* internkey;
-		if(testloc == NULL) {
-			if((nloc+1)<<1 >= sloc) {
-				sloc = ((((nloc+1)>>7)+1)<<8);
-				locations = realloc(locations,sizeof(*locations)*sloc);
-			}
-			locations[nloc].s = malloc(loc.l);
-			internkey = locations[nloc].s;
-			locations[nloc].l = loc.l;
-			memcpy(locations[nloc].s, loc.s, loc.l);
-			printf("CREATE LOCATION %p ",locations[nloc].s);
-			STRPRINT(loc);
-			putchar('\n');
-			ensure0(mergesort(locations,++nloc,sizeof(*locations),(void*)compare_loc));
-		} else {
-			internkey = testloc->s;
-		}
-
-		// now location->s is our interned string key, combine with chapnum to make a unique key
-		// note: this algorithm will process all chapter 1's, then all chapter 2's etc
-
-		/* incidentally, since malloc always returns increasing values if nothing is freed,
-			 the pointer order interned strings will be the order of most recently seen key.
-			 a d b a a a c a b d -> a d b c
-		*/
-
-		int find_chapter(void* ignoredkey, struct chapter* value) {
-			if(chapnum < value->num) return -1;
-			if(chapnum == value->num)
-				if(internkey < value->location.s) // magic
-					return -1;
-				else if(internkey == value->location.s)
-					return 0;
-			return 1;
-		}
-
-		struct chapter* chapter = bsearch(NULL,chapters,nchap,sizeof(*chapters),
-																			(void*)find_chapter);
-		if(chapter == NULL) {
-			// if timestamp <= the earliest time we last went to...
-			if(nchap > 20000) return false;
-			if(nchap+1 >= schap) {
-				schap = ((((nchap+1)>>7)+1)<<8);
-				chapters = realloc(chapters,schap*sizeof(*chapters));
-			}
-			chapter = &chapters[nchap];
-			chapter->num = chapnum;
-			chapter->location.s = internkey; // NOT loc.s
-			chapter->location.l = loc.l; // ...fine
-			/*printf("CREATE CHAPTER %d %p ",chapter->num, internkey);
-			STRPRINT(chapter->location);
-			putchar('\n');*/
-
-			ensure0(mergesort(chapters,++nchap,sizeof(*chapters),(void*)compare_chap));
-		}
+	bool on_commit(git_time_t timestamp, git_tree* last, git_tree* cur) {
+		if(last == NULL) return true;
 		
-		return true;
+		bool on_chapter(long int chapnum,
+										const string loc,
+										const string src) {
+
+			printf("saw %d of ",chapnum);
+			STRPRINT(loc);
+			fputc('\n',stdout);
+			db_saw_chapter(loc,chapnum);
+			return true;
+		}
+		git_for_chapters_changed(last,cur,on_chapter);
 	}
 
 	puts("searching...");
-	git_for_chapters(on_chapter);
+	git_for_commits(on_commit);
+
+
+
+
+	
 	printf("%d locations found\n",nloc);
 
 	int* chaptotal = malloc(sizeof(int*) * nloc);
@@ -187,22 +138,6 @@ int main(int argc, char *argv[])
 		dest.s[LITSIZ("testnew/")+chapter->location.l] = '/';
 		memcpy(dest.s+LITSIZ("testnew/")+chapter->location.l+1,htmlname.s,htmlname.l);
 		dest.s[LITSIZ("testnew/")+chapter->location.l+1 + htmlname.l] = '\0';
-
-		char namebuf[0x100];
-		string name = {
-			.s = namebuf,
-		};
-		name.l = snprintf(name.s,0x100,"chapter%d.hish",chapter->num+1);
-
-		string src = {
-			.l = chapter->location.l + LITSIZ("/markup/") + name.l + 1
-		};
-		// src/dest must be null terminated because open and stat both SUCK
-		src.s = alloca(src.l);
-		memcpy(src.s,chapter->location.s,chapter->location.l);
-		memcpy(src.s + chapter->location.l,LITLEN("/markup/"));
-		memcpy(src.s + chapter->location.l + LITSIZ("/markup/"), name.s, name.l);
-		src.s[chapter->location.l + LITSIZ("/markup/") + name.l] = '\0';
 
 		struct location* testloc = bsearch(&chapter->location,
 																			 locations,nloc,sizeof(*locations),
