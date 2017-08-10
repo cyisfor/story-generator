a0def52ed9ba4d2a619406399fd00af9687a395e
autocommit <autocommit>
2017-08-10 03:52:40 +0000

    auto 0 948 10511

---
+++ source/repo.h
@@ -0,0 +1,10 @@
+
+#include <git2/repository.h>
+#include <git2/errors.h> 
+
+extern git_repository* repo;
+int repo_discover_init(char* start, int len);
+int repo_init(const char* start);
+size_t repo_relative(char** path, size_t plen);
+void repo_check(git_error_code);
+void repo_add(const char* path);
