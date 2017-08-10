1cb18f3cd4007ed3e9282a110213f7b2e57f6b70
autocommit <autocommit>
2017-08-10 21:37:29 +0000

    auto 0 25 376

--- source/create.h
+++ source/create.h
@@ -4,5 +4,5 @@ int create_contents(const string location,
 										const string dest,
 										size_t chapters,
 										void (*with_title)(identifier chapter,
-																			 void(*handle)(const string) title));
+																			 void(*handle)(const string title)));
 void create_chapter(string src, string dest, int chapter, int chapters);
