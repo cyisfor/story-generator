62e49ceaae94ab78fb29e21bff1df9d782ccac7c
autocommit <autocommit>
2017-08-11 01:06:11 +0000

    auto 0 35 538

--- source/db.c
+++ source/db.c
@@ -186,9 +186,13 @@ void db_saw_chapter(bool deleted, identifier story,
 		sqlite3_bind_int64(delete,2,chapter);
 		db_once(delete);
 	} else {
-		DECLARE_STMT(insert,"INSERT OR REPLACE INTO chapters (story,chapter,timestamp) \n"
-								 "SELECT story,chapter,MAX(timestamp,?) FROM chapters\n"
-								 "WHERE story = ? AND chapter = ?");
+		DECLARE_STMT(update,"UPDATE chapters SET timestamp = ? WHERE story = ? AND chapter = ?"); 
+		DECLARE_STMT(insert,"INSERT INTO chapters (timestamp,story,chapter) VALUES (?,?,?)");
+		sqlite3_bind_int64(update,1,timestamp);
+		sqlite3_bind_int64(update,2,story);
+		sqlite3_bind_int64(update,3,chapter);
+		db_once(update);
+		if(db_changes(db) > 0) return;
 		sqlite3_bind_int64(insert,1,timestamp);
 		sqlite3_bind_int64(insert,2,story);
 		sqlite3_bind_int64(insert,3,chapter);
