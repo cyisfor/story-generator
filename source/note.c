c7384916ba3655c80b03aa49490b22847e272d18
autocommit <autocommit>
2017-08-11 22:25:46 +0000

    auto 0 12 143

--- source/note.c
+++ source/note.c
@@ -14,15 +14,15 @@ static void note(const char* file, int line, const char* fmt, ...) {
 }
 
 void infof(const char* file, int line, const char* fmt, ...) {
-	fputs("INFO: ",stderr);
+	fputs("INFO ",stderr);
 	note(file,line,fmt);
 }
 void warnf(const char* file, int line, const char* fmt, ...) {
-	fputs("WARN: ",stderr);
+	fputs("WARN ",stderr);
 	note(file,line,fmt);
 }
 void errorf(const char* file, int line, const char* fmt, ...) {
-	fputs("ERROR: ",stderr);
+	fputs("ERROR ",stderr);
 	note(file,line,fmt);
 	if(getenv("error_nonfatal")) return;
 	abort();
