62f5535e1e8f5a7b228a0e205e157ff3db91391e
autocommit <autocommit>
2017-08-11 22:54:45 +0000

    auto 0 55 612

--- source/db_oid/make.c
+++ source/db_oid/make.c
@@ -16,11 +16,12 @@ int main(int argc, char *argv[])
 			.id = "deadbeefdeadbeef"
 		};
 		void doit(db_oid res) {
-			printf("um %p %p\n",&test.id,&res);
+			printf("um %p %p\n",&test.id,res);
 			if(&test != &res) same = false;
 			else if(&test != ((git_oid*)&res)) same = false;
 			else if(0!=memcmp(test.id,res,sizeof(db_oid))) same = false;
 		}
+		
 		doit(DB_OID(test));
 	}
 	if(same) {
