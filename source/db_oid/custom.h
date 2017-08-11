8ffb65366d5eb0ade521ec2a228082503cd51711
autocommit <autocommit>
2017-08-11 17:58:46 +0000

    auto 0 14 158

--- source/db_oid/custom.h
+++ source/db_oid/custom.h
@@ -15,7 +15,7 @@ const char* db_oid_str(db_oid oid) {
 	return buf;
 }
 
-git_oid* git_oid(db_oid a) {
+git_oid* GIT_OID(db_oid a) {
 	static git_oid temp; // ugh
 	memcpy(temp.id,a,sizeof(db_oid));
 	return &temp;
--- source/db_oid/same.h
+++ source/db_oid/same.h
@@ -1,3 +1,7 @@
 const char* db_oid_str(db_oid oid) {
 	return git_oid_tostr_s((git_oid*)oid);
 }
+
+git_oid* GIT_OID(db_oid a) {
+	return (git_oid*)a;
+}
