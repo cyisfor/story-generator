a4eb6ad2d599df89f2531b54e47c0a82cfaa3b66
autocommit <autocommit>
2017-08-12 06:24:14 +0000

    auto 0 36 500

--- source/ensure.h
+++ source/ensure.h
@@ -1,21 +1,25 @@
 #include "note.h" // error(...)
 
-#define ensure0(test) { int res = (test); \
-	if(res != 0) { \
-		error(#test " %d not zero",res); \
-		abort(); \
+#define ensure0(test) { int res = (test);				\
+		if(res != 0) {															\
+			error(#test " %d not zero",res);					\
+			abort();																	\
+		}																						\
 	}
 
-#define ensure_gt(less,more) { int res1 = (less); \
-	\ int res2 = (more);
-	if(res1 <= res2) { \
-		error(#less " %d < " #more " %d",res1,res2); \
-		abort(); \
-	}
+#define ensure_eq(less,more) { int res1 = (less);		\
+		int res2 = (more);															\
+		if(res1 != res2) {															\
+			error(#less " %d != " #more " %d",res1,res2); \
+			abort();																			\
+		}																								\
+}
 
-#define ensure_eq(less,more) { int res1 = (less); \
-	\ int res2 = (more);
-	if(res1 != res2) { \
-		error(#less " %d != " #more " %d",res1,res2); \
-		abort(); \
-	}
+
+#define ensure_gt(less,more) { int res1 = (less);		\
+		int res2 = (more);															\
+		if(res1 <= res2) {															\
+			error(#less " %d <= " #more " %d",res1,res2); \
+			abort();																			\
+		}																								\
+}
