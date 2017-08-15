85958fc5ad0171762f0661bbd6f7797cb524501c
autocommit <autocommit>
2017-08-15 00:18:03 +0000

    auto 0 13 236

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 . git-tools/funcs.sh
 
+local=/home/code/git-tools
+remote=https://github.com/cyisfor/git-tools.git
+dest=git-tools
+sync
+
 local=/home/code/htmlish
 remote=https://github.com/cyisfor/htmlish.git
 dest=htmlish
