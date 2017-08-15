c15a2579d07b0abb313c7a29ff59d26d117d2406
autocommit <autocommit>
2017-08-15 00:13:57 +0000

    auto 0 70 574

--- setup.sh
+++ setup.sh
@@ -1,36 +1,6 @@
-function sync {
-		if [[ -d $dir ]]; then
-				source=$dir
-				adjremote=1
-		else
-				echo $dir
-				source=$remote
-		fi
-		if [[ -d $dest ]]; then
-				[[ -n "$nocheck" ]] && return
-				cd $dest
-				git pull local master
-				git pull origin  master
-				cd ..
-		else
-				git clone $source $dest
-				if [[ -n "$adjremote" ]]; then
-						cd $dest
-						git remote set-url origin $remote
-						git remote add local $source
-						#git pull
-						cd ..
-				fi
-		fi
-}
+. git-tools/funcs.sh
 
-function uplink {
-		source=$1/$2
-		[[ -L $2 ]] && return;
-		ln -rs $source $2
-}
-
-dir=/home/code/htmlish
+source=/home/code/htmlish
 remote=https://github.com/cyisfor/htmlish.git
 dest=htmlish
 sync
