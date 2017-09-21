475ec50607f3132e4724e0ee5993445be3637ab6
autocommit <autocommit>
2017-09-21 03:40:08 +0000

    auto 0 19 331

--- src/note.c
+++ src/note.c
@@ -1,15 +1,27 @@
+#include "note.h"
+
 #include <stdio.h>
 #include <stdlib.h> // abort, getenv
 #include <stdarg.h> // va_*
 
+struct note_options {
+	bool show_method;
+	bool show_location;
+};
+
+struct note_options note_options = {};
 
 static void note(const char* how, int hlen,
 								 const char* file, int flen,
 								 int line, const char* fmt, va_list arg) {
-	fwrite(how, hlen, 1, stderr);
-	fputc(' ',stderr);
-	fwrite(file, flen, 1, stderr);
-	fprintf(stderr,":%d ",line);
+	if(note_options.show_method) {
+		fwrite(how, hlen, 1, stderr);
+		fputc(' ',stderr);
+	}
+	if(note_options.show_location) {
+		fwrite(file, flen, 1, stderr);
+		fprintf(stderr,":%d ",line);
+	}
 	vfprintf(stderr,fmt,arg);
 	va_end(arg);
 	fputc('\n',stderr);
@@ -34,4 +46,9 @@ HEAD(error) {
 	BODY("ERROR");
 	if(getenv("error_nonfatal")) return;
 	abort();
+}
+
+void note_init(void) {
+	note_options.show_method = (NULL==getenv("note.hidemethod"));
+	note_options.show_location = (NULL!=getenv("note.location"));
 }
