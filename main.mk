e1fd355e4264733d86d8d6b11d0d0eef922084c2
autocommit <autocommit>
2017-11-11 08:39:34 +0000

    auto 0 95 1176

--- main.mk
+++ main.mk
@@ -1,5 +1,3 @@
-VPATH = o
-
 P=libgit2 sqlite3
 PKG_CONFIG_PATH:=/custom/libgit2/lib/pkgconfig
 export PKG_CONFIG_PATH
@@ -7,142 +5,105 @@ export PKG_CONFIG_PATH
 LIBXML:=libxml2
 XMLVERSION:=include/libxml/xmlversion.h
 
-CFLAGS+=-ggdb -fdiagnostics-color=always $(shell pkg-config --cflags $(P))
+CFLAGS+=-ggdb
 CFLAGS+=-I. -Iddate/ -Ihtmlish/src -Ihtml_when/src -Ihtml_when/ -Ilibxml2/include -Ictemplate/src -Icystuff/src
-LDLIBS+=-lbsd $(shell pkg-config --libs $(P))
+LDLIBS+=-lbsd
 LDLIBS+=$(shell xml2-config --libs | sed -e's/-xml2//g')
-
-derp: setup
-	$(MAKE) all
 
 all: generate test_git describe make-log list-commits set-censored statements2source
 
-LINK=$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ $(LDLIBS)
-COMPILE=$(CC) $(CFLAGS) -c -o $@ $<
-
-# bleah, accumulation side effects...
-O=$(patsubst %,o/%.o,$N) ddate/ddate.o htmlish/libhtmlish.a\
-$(foreach name,$(N),$(eval targets:=$$(targets) $(name)))
-S=$(patsubst %,src/%.c,$N)
-
-N=statements2source db itoa cystuff/mmapfd note
-statements2source: $O
-	$(LINK)
+N=statements2source db itoa cystuff/mmapfile note
+OUT=statements2source
+$(eval $(PROGRAM))
 
 N=cystuff/mmapfd
-$O: cystuff/src/mmapfile.c | o/cystuff
+$(OBJ): cystuff/src/mmapfile.c | $(O)/cystuff
 	$(COMPILE)
 
 o/cystuff: | o/
 	mkdir $@
 
 N=main storygit repo create itoa db note category.gen
-generate: $O
-	$(LINK)
+OUT=generate
+$(eval $(PROGRAM))
 
 N=make-log itoa db note
-make-log: $O
-	$(LINK)
+OUT=make-log
+$(eval $(PROGRAM))
 
 N=make-log
-$(O): o/template/make-log.html.c
+$(OBJ): o/template/make-log.html.c
+	$(COMPILE)
 
-o/template/%.c: template/% ctemplate/generate | o/template
+$(O)/template/%.c: template/% ctemplate/generate | $(O)/template
 	./ctemplate/generate < $< >$@.temp
 	mv $@.temp $@
 
-o/template: | o
+$(O)/template: | $(O)
 	mkdir $@
 
-ctemplate/generate: | ctemplate
+$(TOP)/ctemplate/generate: | ctemplate
 	$(MAKE) -C ctemplate generate
 
-ctemplate: setup
+$(TOP)/ctemplate: setup
 
 N=list-commits storygit repo note itoa
-list-commits: $O
-	$(LINK)
+OUT=list-commits
+$(eval $(PROGRAM))
 
 N=describe itoa db note
-describe: $O 
-	$(LINK) -lreadline
+OUT=describe
+$(eval $(PROGRAM))
 
 N=test_git storygit repo itoa db note
-test_git: $O
-	$(LINK)
+OUT=test_git
+$(eval $(PROGRAM))
 
 N=set-censored db note itoa
-set-censored: $O
-	$(LINK)
+OUT=set-censored
+$(eval $(PROGRAM))
 
-
-
-o/%.d: src/%.c  $(LIBXML)/$(XMLVERSION) | o
-	(echo -n o/; \
-		$(CC) $(CFLAGS) -MG -MM -o - $< ) > $@.temp
-	mv $@.temp $@
-
-o/%.o: src/%.c | o
-	$(COMPILE)
-
-o/category.gen.c o/category.gen.h: src/categories.list ./str_to_enum_trie/main
+$(O)/category.gen.c $(O)/category.gen.h: src/categories.list ./str_to_enum_trie/main
 	(cd o && noupper=1 prefix=category enum=CATEGORY exec ../str_to_enum_trie/main)<$<
 
-o/category.gen.o: o/category.gen.c
+$(O)/category.gen.lo: $(O)/category.gen.c
 	$(COMPILE)
 
-o/main.o: o/category.gen.c o/category.gen.h
+$(O)/main.lo: $(O)/category.gen.c $(O)/category.gen.h
 
-./str_to_enum_trie/main: descend
+./str_to_enum_trie/main: | setup
 	$(MAKE) -C str_to_enum_trie main
 
-o/db.o: o/db-sql.gen.c src/db_oid/gen.h src/db_oid/make.c
+$(O)/db.o: $(O)/db-sql.gen.c src/db_oid/gen.h src/db_oid/make.c
 
-o/%.sql.gen.c: src/%.sql o/make-sql
-	./o/make-sql <$< >$@.temp
+$(O)/%.sql.gen.c: src/%.sql $(O)/make-sql
+	./$(O)/make-sql <$< >$@.temp
 	mv $@.temp $@
 
-o/%.ctempl.c: src/% ctemplate/generate
+$(O)/%.ctempl.c: src/% ctemplate/generate
 	./ctemplate/generate <$< >$@.temp
 	mv $@.temp $@
 
-src/db_oid/gen.h: o/db_oid/make | src/db_oid/same.h src/db_oid/custom.h
-	cd src/db_oid && ../../o/db_oid/make
+src/db_oid/gen.h: $(O)/db_oid/make | src/db_oid/same.h src/db_oid/custom.h
+	cd src/db_oid && ../../$(O)/db_oid/make
 
-o/db_oid/make: o/db_oid/make.o
-o/db_oid/make.o: | o/db_oid
+$(O)/db_oid/make: $(O)/db_oid/make.o
+$(O)/db_oid/make.o: | $(O)/db_oid
 
-o/make-sql: o/make-sql.o
+$(O)/make-sql: $(O)/make-sql.o
 	$(LINK)
 
-ddate/ddate.o:
+o/ddate.o:
 	$(MAKE) -C ddate ddate.o
+	mv ddate/ddate.o $@
 
-o o/db_oid:
+$(O)/db_oid: | $(O)
 	mkdir $@
 
-clean:
-	rm -rf o generderp
-
-htmlish/libhtmlish.a: descend
-
-descend:
-	$(MAKE) -C htmlish libhtmlish.a
-
-.PHONY: descend setup
+.PHONY: setup
 
 $(LIBXML)/$(XMLVERSION): descend
 
 setup:
 	sh setup.sh
 	$(MAKE) -C htmlish setup
-
-git-tools/pushcreate: git-tools/pushcreate.c
-	$(MAKE) -C git-tools
-
-push: setup git-tools/pushcreate
-	[[ -n "$(remote)" ]]
-	./git-tools/pushcreate "$(remote)"
-	$(MAKE) -C htmlish push remote="$(remote)/htmlish"
-
--include $(patsubst %, o/%.d,$(targets))
