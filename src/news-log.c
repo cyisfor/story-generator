c952b6ed9525b9de4c2ebc628b67ec53f3ba2e10
autocommit <autocommit>
2018-04-15 08:15:34 +0000

    auto 0 23 311

--- src/news-log.c
+++ src/news-log.c
@@ -1,4 +1,6 @@
-#include "storygit.h"
+#include "repo.h"
+#include <stdio.h>
+
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +9,11 @@ int main(int argc, char *argv[])
 	git_revwalk* walk;
 	repo_check(git_revwalk_new(&walk, repo));
 	git_revwalk_sorting(GIT_SORT_TIME);
+
+	puts("<!DOCTYPE html>\n"
+			 "<html><head><meta charset=\"utf-8\"/>\n"
+			 "<title>News log</title>\n"
+			 "</head><body>\n");
 	
 	// since HEAD
 	git_reference* ref;
@@ -28,21 +35,16 @@ int main(int argc, char *argv[])
 					puts("<p>");
 					time_t time = git_commit_time(commit);
 					puts(ctime(&time));
-					puts(" ");
+					puts("<b>");
 					puts(git_commit_summary(commit));
 					puts("</b></p><p>");
 					puts(message);
 					puts("</p>");
+					if(++count > 256) break;
 				}
-				char* nl = strchr(message, '\n');
-				if(nl) {
-					nl = strchr(nl, '\n');
-					if(nl) {
-						
-				
+			}
 		}
-
-
-	
+	}
+	puts("</body></html>");
 	return 0;
 }
