7bd84826fd9bc172e271b54eec44391d636e9686
autocommit <autocommit>
2018-04-15 08:13:24 +0000

    auto 0 53 768

--- src/news-log.c
+++ src/news-log.c
@@ -10,12 +10,38 @@ int main(int argc, char *argv[])
 	
 	// since HEAD
 	git_reference* ref;
-	git_commit* commit;
 	repo_check(git_repository_head(&ref, repo)); // this resolves the reference
 	const git_oid * oid = git_reference_target(ref);
 	git_reference_free(ref);
 
 	git_revwalk_push(walk, oid);
+
+	while(!git_revwalk_next(&oid, walk)) {
+		git_commit* commit = NULL;
+		repo_check(git_commit_lookup(&commit, repo, oid));
+		const git_signature* sig = git_commit_author(commit);
+		int nlen = strlen(sig->name);
+		if(nlen == sizeof("Skybrook")-1) {
+			if(0==memcmp(sig->name, "Skybrook", nlen)) {
+				const char* message = git_commit_body(commit);
+				if(message != NULL) {
+					puts("<p>");
+					time_t time = git_commit_time(commit);
+					puts(ctime(&time));
+					puts(" ");
+					puts(git_commit_summary(commit));
+					puts("</b></p><p>");
+					puts(message);
+					puts("</p>");
+				}
+				char* nl = strchr(message, '\n');
+				if(nl) {
+					nl = strchr(nl, '\n');
+					if(nl) {
+						
+				
+		}
+
 
 	
 	return 0;
